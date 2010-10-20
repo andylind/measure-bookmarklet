a09ecd8aab7ef93f5a8bdacf85a1bb4079436ddc
andylind <andrewrlind@gmail.com>
2010-10-20 18:57:23 -0500

    changed display of x/y

--- measure.js
+++ measure.js
@@ -36,26 +36,28 @@ measurebookmarklet = (function(){
         headID.appendChild(cssNode);
     };
 
-    getMousePosition = function(e) {
-	    var x = e.pageX,
-		    y = e.pageY;
-        document.Show.MouseX.value = x;
-        document.Show.MouseY.value = y;
-    };
+
     
     loadToolbar = function (){
         if (!document.getElementById("measure-toolbar")){
             loadCSS('measure-toolbar-css', 'http://localhost:8000/measure.css');
         }
-        document.onmousemove = getMousePosition;
+
 
         if (!document.getElementById("measure-toolbar")){
             var measureToolbar = document.createElement('div');
             measureToolbar.id = "measure-toolbar";
             document.body.appendChild(measureToolbar);
-            document.getElementById("measure-toolbar").innerHTML = '<div id="measure-toolbar-header">Measure</div><div id="measure-toolbar-close"><a id="measure-toolbar-close-link" href="#">x</a></div><br><br><div><form name="Show">&nbsp;X:&nbsp;<input type="text" name="MouseX" value="0" size="4"><br>&nbsp;Y:&nbsp;<input type="text" name="MouseY" value="0" size="4"><br></form></div>';
+            document.getElementById("measure-toolbar").innerHTML = '<div id="measure-toolbar-header">Measure</div><div id="measure-toolbar-close"><a id="measure-toolbar-close-link" href="#">x</a></div><br><br>X:<span id="measure-toolbar-xvalue">0</span<br>Y:<span id="measure-toolbar-yvalue">0</span>';
             document.getElementById("measure-toolbar-close-link").setAttribute('onclick', 'measurebookmarklet.removeToolbar()');
         }
+        
+        document.onmousemove = function(e) {
+	        var x = e.pageX,
+		        y = e.pageY;
+            document.getElementById("measure-toolbar-xvalue").innerHTML = x;
+            document.getElementById("measure-toolbar-yvalue").innerHTML = y;
+        };
     };
     
     return {
